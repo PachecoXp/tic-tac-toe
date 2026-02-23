61d2b29a37a166963451593a43ead37b89f88b45
Evaristo Tapia Pacheco <tucorreo@example.com>
2026-02-22 21:15:29 -0500

    feat: logica implementada

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ class TicTacToe:
     def __init__(self, root):
 
         self.root = root
-        self.root.title("Tic Tac Toe - Práctica Git")
-
+        self.root.title("Tic Tac Toe - Logic Version")
         self.turno = "X"
         self.tablero = [""] * 9
         self.botones = []
